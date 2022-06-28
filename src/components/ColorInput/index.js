576a62c398b313aa3c31154a9f3e2899c57a7bd8
Thomas Cleary <thomasclearydev@gmail.com>
2022-06-28 16:38:55 +0800

    Add validation for color inputs

--- src/components/ColorInput/index.js
+++ src/components/ColorInput/index.js
@@ -1,20 +1,34 @@
 import './index.css';
 
+
+const isValidRgbValue = (input) => {
+  const isNotNan = !Number.isNaN(input);
+  const isInRgbRange = input <= 255 && input >= 0;
+
+  return isNotNan && isInRgbRange;
+}
+
+
 const ColorInput = (props) => {
   const onNumberChange = (event) => {
-    props.onChange(event.target.value);
+    const input = Number(event.target.value);
+    isValidRgbValue(input)
+      ? props.onChange(input)
+      : console.log('Invalid input');
   };
 
   return (
     <div className='color-input'>
-      <h2 className='color-input-heading'>{ props.label }</h2>
+      <h2 
+      className='color-input-heading'
+      style={{color: props.inputColor}}
+      >{ props.label }</h2>
       <input 
       className='number-input' 
+      value={props.value}
       type='tel'  
       maxLength='3'
       minLength='1'
-      style={{backgroundColor: props.color}}
-      value={props.value}
       onChange={onNumberChange}
       />
     </div>
--- src/components/ColorInputs/index.js
+++ src/components/ColorInputs/index.js
@@ -6,19 +6,19 @@ const ColorInputs = (props) => {
   return (
     <div className='color-inputs dark-bg'>
       <ColorInput 
-      color={props.inputColor} 
+      inputColor={props.inputColor} 
       value={props.values.red} 
       label='R' 
       onChange={props.onChange.setRed}
       />
       <ColorInput 
-      color={props.inputColor} 
+      inputColor={props.inputColor} 
       value={props.values.green} 
       label='G' 
       onChange={props.onChange.setGreen}
       />
       <ColorInput 
-      color={props.inputColor} 
+      inputColor={props.inputColor} 
       value={props.values.blue} 
       label='B' 
       onChange={props.onChange.setBlue}
